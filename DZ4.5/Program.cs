061e42faa22445563aa00a0d2c4a86183b9e77a4
balakinsergey83 <balakinsergey83@gmail.com>
2018-02-20 00:18:54 +0200

    DZ4.5 вариант вложенный if

--- DZ4.5/Program.cs
+++ DZ4.5/Program.cs
@@ -10,6 +10,22 @@ namespace DZ4._5
     {
         static void Main(string[] args)
         {
+            float a1 = float.Parse(Console.ReadLine());
+            float a = 20.50f;
+            float b = 30.75f;
+
+            if (a1 < a)
+            { Console.WriteLine("ВНИМАНИЕ ЦЕНА ОПУСТИЛАСЬ!!!"); }
+
+            else if (a1 > b)
+            { Console.WriteLine("ВНИМАНИЕ ЦЕНА ПОДНЯЛАСЬ!!!"); }
+
+            else if (a1 >= a || a1 <= b)
+            { Console.WriteLine("Все ОК"); }
+
+
+            Console.Read();
+
         }
     }
 }
